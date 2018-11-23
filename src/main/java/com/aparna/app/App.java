bc9f57e7f267d7c3ecd3fa6735ff8f21aa9f6e41
aparnal <laxmiaparna.atmakuru@alacriti.com>
2018-11-23 12:11:20 +0530

    initial commit

---
+++ src/main/java/com/aparna/app/App.java
@@ -0,0 +1,13 @@
+package com.aparna.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
